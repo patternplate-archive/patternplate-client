823a27bf693b09691e4119ff1dbfc9ae3187077f
Mario Nebl <mario.nebl@sinnerschrader.com>
2015-05-14 23:44:08 +0200

    Use cache on api route if applicable

--- source/application/routes/api.js
+++ source/application/routes/api.js
@@ -8,19 +8,33 @@ function apiRouteFactory (application) {
 
 		let clientPath = config.path[config.path.length - 1] === '/' ? config.path : `${config.path}/`
 		let base = `http://${config.host}:${config.port}${clientPath}`;
+		let uri = `${base}${path}`;
 
-		try {
-			let response = await fetch(`${base}${path}`, {'headers': {'accept-type':'application/json'}});
-			let data = await response.json();
+		let data;
 
-			if (response.status < 400) {
-				this.body = data;
-			} else {
-				throw new Error(data.message || `Request to ${base}/${path} failed`, data.error || {});
-			}
-		} catch (err) {
-			this.throw(err, 500);
+		if (application.cache) {
+			data = application.cache.get(uri);
 		}
+
+		if (! data) {
+			try {
+				let response = await fetch(uri, {'headers': {'accept-type':'application/json'}});
+				data = await response.json();
+
+				if (application.cache) {
+					application.cache.set(uri, data);
+				}
+
+				if (response.status >= 400) {
+					throw new Error(data.message || `Request to ${base}/${path} failed`, data.error || {});
+				}
+			} catch (err) {
+				this.throw(err, 500);
+			}
+		}
+
+
+		this.body = data;
 	};
 }
 
