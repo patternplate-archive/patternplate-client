c8b16a09b704ca058149ec74e8587fdfa42d78f0
Mario Nebl <mario.nebl@sinnerschrader.com>
2015-05-15 00:32:25 +0200

    Version bump, caching

--- application/routes/api.js
+++ application/routes/api.js
@@ -10,7 +10,7 @@ require('isomorphic-fetch');
 
 function apiRouteFactory(application) {
 	return function apiRoute() {
-		var config, path, clientPath, base, response, data;
+		var config, path, clientPath, base, uri, data, response;
 		return regeneratorRuntime.async(function apiRoute$(context$2$0) {
 			while (1) switch (context$2$0.prev = context$2$0.next) {
 				case 0:
@@ -18,45 +18,60 @@ function apiRouteFactory(application) {
 					path = this.params.path;
 					clientPath = config.path[config.path.length - 1] === '/' ? config.path : '' + config.path + '/';
 					base = 'http://' + config.host + ':' + config.port + '' + clientPath;
-					context$2$0.prev = 4;
-					context$2$0.next = 7;
-					return fetch('' + base + '' + path, { 'headers': { 'accept-type': 'application/json' } });
+					uri = '' + base + '' + path;
+					data = undefined;
 
-				case 7:
-					response = context$2$0.sent;
-					context$2$0.next = 10;
-					return response.json();
+					if (application.cache) {
+						data = application.cache.get(uri);
+					}
 
-				case 10:
-					data = context$2$0.sent;
-
-					if (!(response.status < 400)) {
-						context$2$0.next = 15;
+					if (data) {
+						context$2$0.next = 23;
 						break;
 					}
 
-					this.body = data;
-					context$2$0.next = 16;
-					break;
+					context$2$0.prev = 8;
+					context$2$0.next = 11;
+					return fetch(uri, { 'headers': { 'accept-type': 'application/json' } });
 
-				case 15:
+				case 11:
+					response = context$2$0.sent;
+					context$2$0.next = 14;
+					return response.json();
+
+				case 14:
+					data = context$2$0.sent;
+
+					if (application.cache) {
+						application.cache.set(uri, data);
+					}
+
+					if (!(response.status >= 400)) {
+						context$2$0.next = 18;
+						break;
+					}
+
 					throw new Error(data.message || 'Request to ' + base + '/' + path + ' failed', data.error || {});
 
-				case 16:
-					context$2$0.next = 21;
+				case 18:
+					context$2$0.next = 23;
 					break;
 
-				case 18:
-					context$2$0.prev = 18;
-					context$2$0.t3 = context$2$0['catch'](4);
+				case 20:
+					context$2$0.prev = 20;
+					context$2$0.t3 = context$2$0['catch'](8);
 
 					this['throw'](context$2$0.t3, 500);
 
-				case 21:
+				case 23:
+
+					this.body = data;
+
+				case 24:
 				case 'end':
 					return context$2$0.stop();
 			}
-		}, null, this, [[4, 18]]);
+		}, null, this, [[8, 20]]);
 	};
 }
 
