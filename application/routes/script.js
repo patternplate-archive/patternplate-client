37c86c236e01457fb8a570a4e496fb74550fa235
Mario Nebl <mario.nebl@sinnerschrader.com>
2015-06-12 11:08:17 +0200

    Prebuild frontend assets, make /script/ fast.

--- application/routes/script.js
+++ application/routes/script.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, '__esModule', {
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i]; return arr2; } else { return Array.from(arr); } }
+var _fs = require('fs');
 
 var _path = require('path');
 
@@ -18,253 +18,54 @@ var _qIoFs = require('q-io/fs');
 
 var _qIoFs2 = _interopRequireDefault(_qIoFs);
 
-var memo = {};
-
-function bundle(bundler) {
-	return new Promise(function (resolve, reject) {
-		bundler.bundle(function (err, buffer) {
-			if (err) {
-				return reject(err);
-			}
-			resolve(buffer);
-		});
-	});
-}
-
-function preBundle(application) {
-	var scripts, files, _iteratorNormalCompletion, _didIteratorError, _iteratorError, _iterator, _step, file, bundler;
-
-	return regeneratorRuntime.async(function preBundle$(context$1$0) {
-		while (1) switch (context$1$0.prev = context$1$0.next) {
-			case 0:
-				if (!(application.configuration.environment === 'production')) {
-					context$1$0.next = 42;
-					break;
-				}
-
-				scripts = (0, _path.resolve)(application.runtime.cwd, 'assets', 'script');
-				context$1$0.next = 4;
-				return regeneratorRuntime.awrap(_qIoFs2['default'].list(scripts));
-
-			case 4:
-				files = context$1$0.sent;
-				_iteratorNormalCompletion = true;
-				_didIteratorError = false;
-				_iteratorError = undefined;
-				context$1$0.prev = 8;
-				_iterator = files[Symbol.iterator]();
-
-			case 10:
-				if (_iteratorNormalCompletion = (_step = _iterator.next()).done) {
-					context$1$0.next = 28;
-					break;
-				}
-
-				file = _step.value;
-				bundler = (0, _browserify2['default'])();
-
-				bundler.transform(babelify.configure({
-					'stage': 0,
-					'ignore': /node_modules/,
-					'optional': ['runtime']
-				}));
-
-				bundler.transform(uglifyify.configure({
-					'global': true
-				}));
-
-				bundler.add((0, _path.resolve)(scripts, file));
-
-				context$1$0.prev = 16;
-				context$1$0.next = 19;
-				return regeneratorRuntime.awrap(bundle(bundler));
-
-			case 19:
-				memo[(0, _path.resolve)(scripts, file)] = context$1$0.sent;
-				context$1$0.next = 25;
-				break;
-
-			case 22:
-				context$1$0.prev = 22;
-				context$1$0.t0 = context$1$0['catch'](16);
-
-				application.log.error(context$1$0.t0);
-
-			case 25:
-				_iteratorNormalCompletion = true;
-				context$1$0.next = 10;
-				break;
-
-			case 28:
-				context$1$0.next = 34;
-				break;
-
-			case 30:
-				context$1$0.prev = 30;
-				context$1$0.t1 = context$1$0['catch'](8);
-				_didIteratorError = true;
-				_iteratorError = context$1$0.t1;
-
-			case 34:
-				context$1$0.prev = 34;
-				context$1$0.prev = 35;
-
-				if (!_iteratorNormalCompletion && _iterator['return']) {
-					_iterator['return']();
-				}
-
-			case 37:
-				context$1$0.prev = 37;
-
-				if (!_didIteratorError) {
-					context$1$0.next = 40;
-					break;
-				}
-
-				throw _iteratorError;
-
-			case 40:
-				return context$1$0.finish(37);
-
-			case 41:
-				return context$1$0.finish(34);
-
-			case 42:
-			case 'end':
-				return context$1$0.stop();
-		}
-	}, null, this, [[8, 30, 34, 42], [16, 22], [35,, 37, 41]]);
-}
-
-function applyTransforms(bundler, transformNames, transformConfigs, transformDictionary) {
-	var _iteratorNormalCompletion2 = true;
-	var _didIteratorError2 = false;
-	var _iteratorError2 = undefined;
-
-	try {
-		for (var _iterator2 = transformNames[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-			var transformName = _step2.value;
-
-			/*eslint-disable no-continue*/
-			var transformFn = transformDictionary[transformName];
-			var args = undefined;
-
-			if (!transformFn) {
-				continue;
-			}
-
-			if (typeof transformFn.configure === 'function') {
-				args = [transformFn.configure(transformConfigs[transformName])];
-			} else {
-				args = [transformFn, transformConfigs[transformName]];
-			}
-
-			bundler.transform.apply(bundler, _toConsumableArray(args));
-		}
-	} catch (err) {
-		_didIteratorError2 = true;
-		_iteratorError2 = err;
-	} finally {
-		try {
-			if (!_iteratorNormalCompletion2 && _iterator2['return']) {
-				_iterator2['return']();
-			}
-		} finally {
-			if (_didIteratorError2) {
-				throw _iteratorError2;
-			}
-		}
-	}
-
-	return bundler;
-}
-
 function scriptRouteFactory(application) {
 	var browserifyConfig = application.configuration.assets.browserify || {};
 
-	var transformNames = Object.keys(browserifyConfig.transforms).filter(function (transformName) {
-		return browserifyConfig.transforms[transformName].enabled;
-	});
-
-	var transformConfigs = transformNames.reduce(function getTransformConfig(results, transformName) {
-		results[transformName] = browserifyConfig.transforms[transformName].opts || {};
-		return results;
-	}, {});
-
-	var transformFns = transformNames.reduce(function getTransformFns(results, transformName) {
-		results[transformName] = require(require.resolve(transformName));
-		return results;
-	}, {});
-
-	var useTransforms = function useTransforms(bundler) {
-		return applyTransforms(bundler, transformNames, transformConfigs, transformFns);
-	};
-
-	try {
-		preBundle(application);
-	} catch (err) {
-		application.log.warn('Prebundling of scripts failed');
-		application.log.trace(err);
-	}
-
 	return function scriptRoute() {
-		var path, bundler;
+		var suffix, ext, filename, relative, path, bundler;
 		return regeneratorRuntime.async(function scriptRoute$(context$2$0) {
 			while (1) switch (context$2$0.prev = context$2$0.next) {
 				case 0:
-					path = (0, _path.resolve)(application.runtime.cwd, 'assets', 'script', this.params.path || '');
-					context$2$0.next = 3;
+					suffix = application.runtime.env === 'development' ? '' : 'bundle';
+					ext = (0, _path.extname)(this.params.path).slice(1);
+					filename = [(0, _path.basename)(this.params.path, '.' + ext), suffix, ext].filter(function (fragment) {
+						return fragment;
+					}).join('.');
+					relative = (0, _path.dirname)(this.params.path);
+					path = (0, _path.resolve)(application.runtime.cwd, 'assets', 'script', relative, filename);
+					context$2$0.next = 7;
 					return regeneratorRuntime.awrap(_qIoFs2['default'].exists(path));
 
-				case 3:
+				case 7:
 					if (context$2$0.sent) {
-						context$2$0.next = 5;
-						break;
-					}
-
-					return context$2$0.abrupt('return');
-
-				case 5:
-
-					this.type = 'js';
-
-					if (!(application.configuration.environment === 'production' && memo[path])) {
 						context$2$0.next = 9;
 						break;
 					}
 
-					this.body = memo[path];
 					return context$2$0.abrupt('return');
 
 				case 9:
-					bundler = (0, _browserify2['default'])();
 
-					useTransforms(bundler);
-					bundler.add(path);
+					this.type = 'js';
 
-					context$2$0.prev = 12;
-					context$2$0.next = 15;
-					return regeneratorRuntime.awrap(bundle(bundler));
+					try {
+						if (application.runtime.env === 'development') {
+							bundler = (0, _browserify2['default'])(path, browserifyConfig);
 
-				case 15:
-					memo[path] = context$2$0.sent;
+							this.body = bundler.bundle();
+						} else {
+							this.body = (0, _fs.createReadStream)(path);
+						}
+					} catch (err) {
+						application.log.error(err);
+						this['throw'](err, 500);
+					}
 
-					this.body = memo[path];
-					context$2$0.next = 23;
-					break;
-
-				case 19:
-					context$2$0.prev = 19;
-					context$2$0.t0 = context$2$0['catch'](12);
-
-					application.log.error(context$2$0.t0);
-					this['throw'](context$2$0.t0, 500);
-
-				case 23:
+				case 11:
 				case 'end':
 					return context$2$0.stop();
 			}
-		}, null, this, [[12, 19]]);
+		}, null, this);
 	};
 }
 
--- source/configuration/assets.js
+++ source/configuration/assets.js
@@ -1,20 +1,8 @@
 const assets = {
 	'browserify': {
-		'transforms': {
-			'babelify': {
-				'enabled': true,
-				'opts': {
-					'stage': 0,
-					'optional': ['runtime']
-				}
-			},
-			'uglifyify': {
-				'enabled': false,
-				'opts': {
-					'global': true
-				}
-			}
-		}
+		'noParse': ['react', 'react-router'],
+		'detectGlobals': false,
+		'insertGlobals': true
 	},
 	'less': {
 
