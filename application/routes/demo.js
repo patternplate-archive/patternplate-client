d1d8bb1de409e7772b1db77494554503a4832602
Mario Nebl <mario.nebl@sinnerschrader.com>
2015-06-25 16:00:36 +0200

    Fix demo proxy route for independent server/client setups, fixes #17

--- application/routes/demo.js
+++ application/routes/demo.js
@@ -14,19 +14,24 @@ var _koaProxy2 = _interopRequireDefault(_koaProxy);
 
 function apiRouteFactory(application) {
 	return regeneratorRuntime.mark(function apiRoute(next) {
-		var config, host;
+		var config, serverConfig, serverHost, clientHost, proxied, path, host;
 		return regeneratorRuntime.wrap(function apiRoute$(context$2$0) {
 			while (1) switch (context$2$0.prev = context$2$0.next) {
 				case 0:
 					config = application.configuration.client;
-					host = 'http://' + config.host + ':' + config.port + '/api/pattern/';
+					serverConfig = application.configuration.server;
+					serverHost = '' + serverConfig.host + ':' + serverConfig.port;
+					clientHost = '' + config.host + ':' + config.port;
+					proxied = clientHost === serverHost;
+					path = proxied ? '/api/pattern/' : '/pattern/';
+					host = 'http://' + config.host + ':' + config.port + '' + path;
 
 					this.path = this.path.split('/').slice(2).join('/');
 
-					context$2$0.next = 5;
+					context$2$0.next = 10;
 					return (0, _koaProxy2['default'])({ host: host }).call(this, next);
 
-				case 5:
+				case 10:
 				case 'end':
 					return context$2$0.stop();
 			}
--- source/application/routes/demo.js
+++ source/application/routes/demo.js
@@ -4,8 +4,14 @@ import proxy from 'koa-proxy';
 function apiRouteFactory (application) {
 	return function * apiRoute (next) {
 		let config = application.configuration.client;
-		let host = `http://${config.host}:${config.port}/api/pattern/`;
+		let serverConfig = application.configuration.server;
 
+		let serverHost = `${serverConfig.host}:${serverConfig.port}`;
+		let clientHost = `${config.host}:${config.port}`;
+
+		let proxied = clientHost === serverHost;
+		let path = proxied ? '/api/pattern/' : '/pattern/';
+		let host = `http://${config.host}:${config.port}${path}`;
 		this.path = this.path.split('/').slice(2).join('/');
 
 		yield proxy({host}).call(this, next);
