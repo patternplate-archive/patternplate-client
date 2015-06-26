7586e30d20c5a2e17d27e3c4e476003374e57d32
dangoo <daniel.gooss@sinnerschrader.com>
2015-06-26 16:03:40 +0200

    built files for npm usage

--- application/components/common/icon.js
+++ application/components/common/icon.js
@@ -6,6 +6,8 @@ Object.defineProperty(exports, '__esModule', {
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
@@ -24,9 +26,7 @@ var Icon = (function (_React$Component) {
 	function Icon() {
 		_classCallCheck(this, Icon);
 
-		if (_React$Component != null) {
-			_React$Component.apply(this, arguments);
-		}
+		_get(Object.getPrototypeOf(Icon.prototype), 'constructor', this).apply(this, arguments);
 
 		this.displayName = 'Icon';
 	}
@@ -39,7 +39,7 @@ var Icon = (function (_React$Component) {
 			var className = (0, _classnames2['default'])('icon', this.props.className);
 			var textClassName = (0, _classnames2['default'])('svg-text', { 'svg-fallback': this.props.fallback });
 
-			var href = this.props.inline ? '#' + this.props.symbol : '' + this.props.uri + '#' + this.props.symbol;
+			var href = this.props.inline ? '#' + this.props.symbol : this.props.uri + '#' + this.props.symbol;
 			var text = this.props.children ? _react2['default'].createElement(
 				'div',
 				{ className: textClassName },
