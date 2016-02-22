f9fe9767fc62952e5aa774b0a328f05d94c56d57
Mario Nebl <hello@herebecode.com>
2016-02-22 20:20:10 +0100

    fix: make icon and title easily configurable

--- source/application/components/common/icon.jsx
+++ source/application/components/common/icon.jsx
@@ -1,36 +1,63 @@
+import {parse} from 'url';
+
 import React from 'react';
 import classnames from 'classnames';
 import {PropTypes} from 'react';
+
+function isURI(input) {
+	const {host, hostname} = parse(input);
+	return [host, hostname].every(Boolean);
+}
 
 class Icon extends React.Component {
 	displayName = 'Icon';
 
 	static defaultProps = {
-		'uri': '/static/images/icons.svg',
-		'fallback': true,
-		'inline': false
+		uri: '/static/images/icons.svg',
+		fallback: true,
+		inline: false
 	};
 
 	static propTypes = {
-		'uri': PropTypes.string.isRequired,
-		'symbol': PropTypes.string.isRequired,
-		'className': PropTypes.string,
-		'fallback': PropTypes.bool.isRequired,
-		'inline': PropTypes.bool.isRequired
+		uri: PropTypes.string.isRequired,
+		symbol: PropTypes.string.isRequired,
+		className: PropTypes.string,
+		fallback: PropTypes.bool.isRequired,
+		inline: PropTypes.bool.isRequired
 	};
 
-	render () {
-		let className = classnames('icon', this.props.className);
-		let textClassName = classnames('svg-text', {'svg-fallback': this.props.fallback});
+	render() {
+		const className = classnames('icon', this.props.className);
+		const textClassName = classnames(
+			'svg-text',
+			{
+				'svg-fallback': this.props.fallback
+			});
 
-		let href = this.props.inline ? `#${this.props.symbol}` : `${this.props.uri}#${this.props.symbol}`;
-		let text = this.props.children ? <div className={textClassName}>{this.props.children}</div> : '';
+		const fragment = this.props.inline ?
+			`#${this.props.symbol}` :
+			`${this.props.uri}#${this.props.symbol}`;
 
-		let svg = `<svg class="svg" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><use xlink:href="${href}" /></svg>`;
+		const href = isURI(this.props.symbol) ?
+			this.props.symbol :
+			fragment;
+
+		const text = this.props.children ?
+			<div className={textClassName}>{this.props.children}</div> :
+			'';
+
+		const svg = [
+			`<svg class="svg" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">`,
+			`<use xlink:href="${href}" />`,
+			`</svg>`
+		].join('');
 
 		return (
 			<div className={className}>
-				<div className="svg-icon" dangerouslySetInnerHTML={{'__html': svg}} />
+				<div
+					className="svg-icon"
+					dangerouslySetInnerHTML={{__html: svg}}
+					/>
 				{text}
 			</div>
 		);
--- source/application/components/navigation/toolbar.jsx
+++ source/application/components/navigation/toolbar.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import classnames from 'classnames';
+import cookie from 'cookie';
 import {Link} from 'react-router';
 
-import cookie from 'cookie';
-
 import Icon from '../common/icon';
+
+const document = global.document;
 
 class Toolbar extends React.Component {
 	displayName = 'Toolbar';
 
 	componentWillMount() {
 		this.setState({
-			'theme': this.props.config.theme,
-			'themeTarget': this.props.config.themeTarget
+			theme: this.props.config.theme,
+			themeTarget: this.props.config.themeTarget
 		});
 	}
 
@@ -21,27 +22,23 @@ class Toolbar extends React.Component {
 	}
 
 	toggleTheme() {
-		let state = {
-			'theme': this.state.themeTarget,
-			'themeTarget': this.state.theme
+		const state = {
+			theme: this.state.themeTarget,
+			themeTarget: this.state.theme
 		};
 
 		this.setState(state);
 
 		if (document) {
-			let data = {};
-
 			try {
-				let cookieData = JSON.parse(cookie.parse(document.cookie)['patternplate-client'] || {});
-				data = cookieData;
+				const cookieData = JSON.parse(cookie.parse(document.cookie)['patternplate-client'] || {});
+				document.cookie = cookie.serialize('patternplate-client', JSON.stringify(Object.assign({}, cookieData, state)));
 			} catch (err) {
 				console.warn('Failed to read data from "patternplate-client" cookie');
 			}
-
-			document.cookie = cookie.serialize('patternplate-client', JSON.stringify(Object.assign({}, data, state)));
 		}
 
-		let link = document.createElement('link');
+		const link = document.createElement('link');
 		link.rel = 'stylesheet';
 		link.href = this.link.href.replace(this.state.theme, this.state.themeTarget);
 
@@ -57,19 +54,29 @@ class Toolbar extends React.Component {
 		this.toggleTheme();
 	}
 
-	render () {
-		let themeClassName = classnames('button', this.state.themeTarget);
+	render() {
+		const themeClassName = classnames('button', this.state.themeTarget);
+		const title = this.props.config.title || this.props.schema.name;
+		const icon = this.props.config.icon || 'patternplate';
 
 		return (
 			<header className="main-header">
 				<Link className="logo" to="root">
-					<Icon symbol="patternplate" fallback={false} inline={true}>{ this.props.schema.name }</Icon>
+					<Icon
+						symbol={icon}
+						fallback={false}
+						inline={true}>
+						{title}
+					</Icon>
 				</Link>
 				<div className="toolbar">
 					<label className="button menu" htmlFor="menu-state">
 						<Icon uri="" symbol="patternplate">Menu</Icon>
 					</label>
-					<button className={themeClassName} type="button" onClick={(e) => this.onThemeButtonClick(e)}>
+					<button
+						className={themeClassName}
+						type="button"
+						onClick={e => this.onThemeButtonClick(e)}>
 						<Icon symbol={this.state.themeTarget}>{this.state.themeTarget}</Icon>
 					</button>
 				</div>
