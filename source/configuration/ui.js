0888933fd2082894c53b3375c3e353f4d7167c10
Mario Nebl <hello@herebecode.com>
2016-08-29 19:06:54 +0200

    style: cleanup configuration.ui

--- source/configuration/ui.js
+++ source/configuration/ui.js
@@ -1,64 +1,44 @@
 const ui = {
-	'hierarchy': {
+	hierarchy: {
 		'atoms': {
-			'displayName': 'Atoms',
-			'order': 1,
-			'icon': 'atoms'
+			displayName: 'Atoms',
+			order: 1,
+			icon: 'atoms'
 		},
 		'molecules': {
-			'displayName': 'Molecules',
-			'order': 2,
-			'icon': 'molecules'
+			displayName: 'Molecules',
+			order: 2,
+			icon: 'molecules'
 		},
 		'polymers': {
-			'displayName': 'Polymers',
-			'order': 3,
-			'icon': 'polymers'
+			displayName: 'Polymers',
+			order: 3,
+			icon: 'polymers'
 		},
 		'organisms': {
-			'displayName': 'Organisms',
-			'order': 4,
-			'icon': 'organisms'
+			displayName: 'Organisms',
+			order: 4,
+			icon: 'organisms'
 		},
 		'ecospheres': {
-			'displayName': 'Ecospheres',
-			'order': 5,
-			'icon': 'ecospheres'
+			displayName: 'Ecospheres',
+			order: 5,
+			icon: 'ecospheres'
 		},
 		'atoms/special-atoms': {
-			'displayName': 'Specialized Atoms'
+			displayName: 'Specialized Atoms'
 		}
 	},
-	'resultOrder': [
-    'Dependencies',
+	resultOrder: [
+		'Dependencies',
 		'Markup',
 		'Script',
 		'Style',
 		'Documentation'
 	],
-	'results': {
-    'Dependencies': {
-      'use': 'source'
-    },
-		'Documentation': {
-			'use': 'buffer'
-		},
-		'Markup': {
-			'use': 'buffer'
-		},
-		'Script': {
-			'use': 'demoSource'
-		},
-		'Style': {
-			'use': 'source'
-		}
-	},
-	'fullscreenPatterns': [
-		// '^pages\/' // Strings containing regexes. do not use /regex/ syntax!
-	],
-	'theme': 'light',
-	'themeTarget': 'dark',
-	'useFolderTable': true
+	theme: 'light',
+	themeTarget: 'dark',
+	useFolderTable: true
 };
 
 export default ui;
