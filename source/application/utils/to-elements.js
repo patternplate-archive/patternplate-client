e7f352dbca68d534dbb8552591a8c22e92ae15ab
Mario Nebl <hello@herebecode.com>
2016-09-02 17:53:53 +0200

    fix: guard to-elements against empty input

--- source/application/utils/to-elements.js
+++ source/application/utils/to-elements.js
@@ -3,6 +3,10 @@ import toh from 'hast-to-hyperscript';
 export default toElements;
 
 function toElements(data) {
+	if (!data) {
+		return null;
+	}
+
 	const root = toh(React.createElement, data);
 	return root.props.children;
 }
