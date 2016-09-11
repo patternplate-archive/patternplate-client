018c2278e13ec0ac09b51976b9e7ca526810b393
Mario Nebl <hello@herebecode.com>
2016-09-11 17:56:28 +0200

    fix: render markdown tables manually to avoid react markup warnings

--- source/application/components/common/markdown/render.js
+++ source/application/components/common/markdown/render.js
@@ -1,6 +1,6 @@
 import gh from 'hast-util-sanitize/lib/github';
 import {merge} from 'lodash';
-import React from 'react';
+import React, {Children} from 'react';
 import remark from 'remark';
 import emoji from 'remark-gemoji-to-emoji';
 import vdom from 'remark-vdom';
@@ -17,6 +17,11 @@ function render(source, options) {
 	const h = React.createElement;
 	const headline = wrap(MarkdownHeadline);
 
+	const table = wrap(strictChildren(MarkdownGeneric, ['thead', 'tbody']));
+	const tbody = wrap(strictChildren(MarkdownGeneric, ['tr']));
+	const tr = wrap(strictChildren(MarkdownGeneric, ['td', 'th']));
+	const td = wrap(MarkdownGeneric);
+
 	const link = {base, hash, query, pathname, onHashChange};
 	const code = {highlights, highlight};
 
@@ -28,7 +33,13 @@ function render(source, options) {
 		h3: headline,
 		h4: headline,
 		h5: headline,
-		h6: headline
+		h6: headline,
+		table,
+		thead: tbody,
+		tbody,
+		tr,
+		th: td,
+		td
 	};
 
 	const sanitize = merge({}, gh);
@@ -45,4 +56,19 @@ function render(source, options) {
 		.use(emoji)
 		.process(source)
 		.contents;
+}
+
+function MarkdownGeneric(props) {
+	const Component = props.tagName;
+	return <Component>{props.children}</Component>;
+}
+
+function strictChildren(Component, tagNames) {
+	return props => {
+		const children = Children.toArray(props.children)
+			.filter(child => {
+				return typeof child === 'object' && tagNames.includes(child.props.tagName);
+			});
+		return <Component {...props}>{children}</Component>;
+	};
 }
