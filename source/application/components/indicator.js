8607d0f53b0888d37260f4a781d8d00f4241466b
mario.nebl@sinnerschrader.com <mario.nebl@sinnerschrader.com>
2017-07-30 22:35:21 +0200

    chore: use styled-components as browser dep only

--- source/application/components/indicator.js
+++ source/application/components/indicator.js
@@ -1,106 +1,112 @@
 import React from 'react';
-import styled, {injectGlobal} from 'styled-components';
 
-const Dot = styled.div`
-	position: relative;
-	flex-grow: 0;
-	flex-shrink: 0;
-	height: 7.5px;
-	width: 7.5px;
-	margin: 0 10px;
-	border-radius: 50%;
-	background: #42A5F5;
-	transition: background .4s ease-in-out, opacity .5s ease-in;
-	opacity: ${props => props.status ? 1 : 0};
-	${props => getGlow(props)}
-`;
+if (!('document' in global)) {
+	module.exports = () => null;
+} else {
+	const styled = require('styled-components').default;
+	const {injectGlobal} = require('styled-components');
 
-const Indicator = styled.div`
-	display: flex;
-	align-items: center;
-`;
-
-const Label = styled.div`
-	${props => getOut(props)}
-`;
-
-export default props => {
-	return (
-		<Indicator>
-			<Label status={props.status}>{getLabel(props)}</Label>
-			<Dot status={props.status}/>
-		</Indicator>
-	);
-};
-
-function getGlow(props) {
-	return `
-		&::before {
-			content: '';
-			position: absolute;
-			top: 50%;
-			left: 50%;
-			height: 12px;
-			width: 12px;
-			filter: blur(3px);
-			background: ${props.status === 'stale' ? 'grey' : 'inherit'};
-			transform: translate(-50%, -50%);
-			opacity: .6;
-			${getPulse(props)};
-		}
-	`;
-}
-
-function getOut(props) {
-	if (props.status !== 'loaded') {
-		return;
-	}
-
-	injectGlobal`
-		@keyframes out {
-			to {
-				opacity: 0;
-			}
-		}
+	const Dot = styled.div`
+		position: relative;
+		flex-grow: 0;
+		flex-shrink: 0;
+		height: 7.5px;
+		width: 7.5px;
+		margin: 0 10px;
+		border-radius: 50%;
+		background: #42A5F5;
+		transition: background .4s ease-in-out, opacity .5s ease-in;
+		opacity: ${props => props.status ? 1 : 0};
+		${props => getGlow(props)}
 	`;
 
-	return `
-		animation: out 1s .15s;
-		animation-fill-mode: forwards;
+	const Indicator = styled.div`
+		display: flex;
+		align-items: center;
 	`;
-}
 
-function getLabel(props) {
-	if (!props.status) {
-		return '';
-	}
+	const Label = styled.div`
+		${props => getOut(props)}
+	`;
 
-	return props.status === 'loaded' ? 'synced' : 'syncing';
-}
+	module.exports = props => {
+		return (
+			<Indicator>
+				<Label status={props.status}>{getLabel(props)}</Label>
+				<Dot status={props.status}/>
+			</Indicator>
+		);
+	};
 
-function getPulse(props) {
-	if (props.status !== 'loading') {
-		return;
-	}
-
-	injectGlobal`
-		@keyframes pulse {
-			from {
+	function getGlow(props) {
+		return `
+			&::before {
+				content: '';
+				position: absolute;
+				top: 50%;
+				left: 50%;
+				height: 12px;
+				width: 12px;
+				filter: blur(3px);
+				background: ${props.status === 'stale' ? 'grey' : 'inherit'};
+				transform: translate(-50%, -50%);
 				opacity: .6;
-				transform: translate(-50%, -50%) scale(1);
+				${getPulse(props)};
 			}
-			50% {
-				opacity: 0;
-				transform: translate(-50%, -50%) scale(.75);
-			}
-			to {
-				opacity: .6;
-				transform: translate(-50%, -50%) scale(1);
-			}
-		}
-	`;
+		`;
+	}
 
-	return `
-		animation: pulse 1s infinite;
-	`;
+	function getOut(props) {
+		if (props.status !== 'loaded') {
+			return;
+		}
+
+		injectGlobal`
+			@keyframes out {
+				to {
+					opacity: 0;
+				}
+			}
+		`;
+
+		return `
+			animation: out 1s .15s;
+			animation-fill-mode: forwards;
+		`;
+	}
+
+	function getLabel(props) {
+		if (!props.status) {
+			return '';
+		}
+
+		return props.status === 'loaded' ? 'synced' : 'syncing';
+	}
+
+	function getPulse(props) {
+		if (props.status !== 'loading') {
+			return;
+		}
+
+		injectGlobal`
+			@keyframes pulse {
+				from {
+					opacity: .6;
+					transform: translate(-50%, -50%) scale(1);
+				}
+				50% {
+					opacity: 0;
+					transform: translate(-50%, -50%) scale(.75);
+				}
+				to {
+					opacity: .6;
+					transform: translate(-50%, -50%) scale(1);
+				}
+			}
+		`;
+
+		return `
+			animation: pulse 1s infinite;
+		`;
+	}
 }
