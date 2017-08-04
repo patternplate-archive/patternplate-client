345731c7a41aff0260c341ec8b033b781cbcc894
mario.nebl@sinnerschrader.com <mario.nebl@sinnerschrader.com>
2017-08-04 22:32:22 +0200

    fixup! tune main search styling

--- source/application/containers/search.js
+++ source/application/containers/search.js
@@ -205,9 +205,9 @@ const StyledForm = styled.div`
 	width: 100%;
 	max-width: 800px;
 	margin: 0 auto;
-	padding-top: 60px;
 	background: inherit;
 	border-color: inherit;
+	padding: 60px 30px 0 30px;
 	${
 		props => !props.inline || props.value &&
 			css`
