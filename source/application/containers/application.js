afe0bf36bcc33ba9d0b0d941fa9850d3f59c91bd
mario.nebl@sinnerschrader.com <mario.nebl@sinnerschrader.com>
2017-08-07 12:58:31 +0200

    feat: apply hierarchy config again

--- source/application/containers/application.js
+++ source/application/containers/application.js
@@ -1,11 +1,85 @@
 import frontmatter from 'front-matter';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import {createSelector} from 'reselect';
 import Application from '../components/application';
 
 import {listen, themeLoaded, windowResize} from '../actions';
 
 export default connect(mapProps, mapDispatch)(Application);
+
+const WEIGHTS = {
+	folder: 0,
+	doc: 1,
+	pattern: 2
+};
+
+const selectChildren = config => createSelector(
+	r => r.docs ? r.docs.children : r.children,
+	children => {
+		return children
+			.filter(child => child.manifest.options.hidden !== false)
+			.map(child => {
+				if (child.id in config.hierarchy) {
+					const o = config.hierarchy[child.id];
+					child.manifest.displayName = o.displayName || child.manifest.displayName;
+					child.manifest.options.order = o.order || child.manifest.options.order;
+					child.manifest.options.icon = o.icon || child.manifest.options.icon;
+				}
+				return child;
+			})
+			.sort((a, b) => {
+				const order = (a.manifest.options.order || 0) - (b.manifest.options.order || 0);
+				const weight = (WEIGHTS[a.type] || 0) - (WEIGHTS[b.type] || 0);
+				const comp = a.manifest.displayName.localeCompare(b.manifest.displayName);
+
+				if (order !== 0) {
+					return order;
+				}
+
+				if (weight !== 0) {
+					return weight;
+				}
+
+				return comp;
+			})
+			.map(c => {
+				if (!c.children) {
+					return c;
+				}
+
+				c.children = selectChildren(config)(c);
+				const body = frontmatter(c.contents || '').body;
+
+				if (c.children.length > 0 && !body) {
+					c.to = c.children[0].id;
+				}
+
+				return c;
+			});
+	}
+);
+
+const selectDocs = createSelector(
+	state => state.schema.docs.id,
+	state => state.schema.docs.contents,
+	state => selectChildren(state.config.hierarchy)(state.schema),
+	state => state.schema.docs.manifest || {displayName: 'Documentation'},
+	(id, contents, children, manifest) => ({id, contents, children, manifest})
+);
+
+const selectMeta = createSelector(
+	selectSchema,
+	schema => schema.meta
+);
+
+const selectNavigation = createSelector(
+	state => selectMeta(state),
+	state => state.config,
+	(meta, config) => {
+		return {children: selectChildren(config)(meta)};
+	}
+);
 
 function mapProps(state, own) {
 	return {
@@ -41,63 +115,6 @@ function mapDispatch(dispatch) {
 		onResize: windowResize,
 		onThemeLoaded: themeLoaded
 	}, dispatch);
-}
-
-function selectDocs(state) {
-	return {
-		id: state.schema.docs.id,
-		contents: state.schema.docs.contents,
-		children: selectChildren(state.schema.docs.children),
-		manifest: state.schema.docs.manifest || {
-			displayName: 'Documentation'
-		}
-	};
-}
-
-function selectNavigation(state) {
-	return {
-		children: selectSchema(state).meta.children
-	};
-}
-
-const WEIGHTS = {
-	folder: 0,
-	doc: 1,
-	pattern: 2
-};
-
-function selectChildren(children) {
-	return children
-		.filter(child => child.manifest.options.hidden !== false)
-		.sort((a, b) => {
-			const order = (a.manifest.options.order || 0) - (b.manifest.options.order || 0);
-			const weight = (WEIGHTS[a.type] || 0) - (WEIGHTS[b.type] || 0);
-			const comp = a.manifest.displayName.localeCompare(b.manifest.displayName);
-
-			if (order !== 0) {
-				return order;
-			}
-
-			if (weight !== 0) {
-				return weight;
-			}
-
-			return comp;
-		})
-		.map(c => {
-			if (!c.children) {
-				return c;
-			}
-
-			c.children = selectChildren(c.children);
-			const body = frontmatter(c.contents || '').body;
-
-			if (c.children.length > 0 && !body) {
-				c.to = c.children[0].id;
-			}
-
-			return c;
-		});
 }
 
 function selectDescription(state) {
