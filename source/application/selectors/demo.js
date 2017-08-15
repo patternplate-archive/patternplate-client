d38724835d36a0f454e7f923b5b3039608cd7ede
mario.nebl@sinnerschrader.com <mario.nebl@sinnerschrader.com>
2017-08-15 17:16:44 +0200

    fix: make demo src mechanics work again

--- source/application/selectors/demo.js
+++ source/application/selectors/demo.js
@@ -2,20 +2,26 @@ import {createSelector} from 'reselect';
 import urlQuery from '../utils/url-query';
 import selectItem from './item';
 
+const selectEnv = createSelector(
+	state => state.environment,
+	state => state.schema.envs,
+	(env, envs) => envs.find(e => e.name === env)
+);
+
 export const selectSrc = createSelector(
 	selectItem,
 	state => state.base,
-	state => state.env,
-	(item, base, env) => {
+	selectEnv,
+	state => state.pattern,
+	(item, base, env, pattern) => {
 		if (!item) {
 			return null;
 		}
-
 		const pathname = urlQuery.format({
 			pathname: `${base}demo/${item.id}/index.html`,
-			query: {environment: env}
+			query: {environment: env.name}
 		});
 
-		return `${pathname}?reload-time=${item.reloadTime}`;
+		return `${pathname}?reload-time=${pattern.reloadTime}`;
 	}
 );
